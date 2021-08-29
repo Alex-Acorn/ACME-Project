dfe28f53f0ce30f66b3a9c4eb7ea4351144a1433
Alex Acorn <acorn.alex@gmail.com>
2021-08-29 10:07:20 -0400

    greeting the new branch

--- src/utils/Foo.java
+++ src/utils/Foo.java
@@ -5,7 +5,7 @@ public class Foo {
 	public static void main(String[] args) {
 		System.out.println("howdy");
 		System.out.println("hi from Github");
-
+		System.out.println("hi 'alex' branch, from Eclipse");
 	}
 
 }
